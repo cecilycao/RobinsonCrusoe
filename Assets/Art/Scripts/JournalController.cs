1adea91cd0925c9e7bb6ed13c89936047e876805
RyouTomokin <55241756+RyouTomokin@users.noreply.github.com>
2020-10-18 22:34:02 +0800

    2020-10-18美术素材更新
    日记本素材更新
    日记本的打开关闭功能完善
    添加人物头像
    更新浮岛模型和贴图
    添加破损浮岛模型

---
+++ Assets/Art/Scripts/JournalController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalController : MonoBehaviour
+{
+    public Animator journalAnim;
+    public GameObject buttons;
+    public GameObject journalIcon;
+    
+    
+    public void JournalOpen()
+    {
+        buttons.SetActive(false);
+        journalAnim.SetBool("OPEN",true);
+        Invoke("ButtonsShow",0.8f);
+    }
+    private void ButtonsShow()
+    {
+        buttons.SetActive(true);
+    }
+
+    public void JournalClose()
+    {
+        buttons.SetActive(false);
+        journalAnim.SetBool("OPEN",false);
+        Invoke("JournalHide",0.8f);
+    }
+    private void JournalHide()
+    {
+        journalAnim.gameObject.SetActive(false);
+        journalIcon.gameObject.SetActive(true);
+    }
+}
