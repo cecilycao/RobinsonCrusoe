5b24fb2f74ca942630f27aac9c40a93bc9c99b29
OldMoM <1090347251@qq.com>
2020-10-26 09:39:21 +0800

    测试

--- Assets/LvPeixi/Managers/Legislator.cs
+++ Assets/LvPeixi/Managers/Legislator.cs
@@ -14,27 +14,38 @@ public class Legislator : MonoBehaviour
     {
         GameEvents.Sigton.onGameEnd += () =>
         {
-            int coreIsland = IslandManager.Instance.m_islandSet.coreIslands.Count;
-            int normal = 0;
-            foreach (var item in IslandManager.Instance.m_islandSet.normalIslands)
-            {
-                if (item.isActive())
-                {
-                    normal++;
-                }
-            }
-            int all = coreIsland + normal;
-            if (all >= 8)
-            {
-                GameEvents.Sigton.GetEvent<Subject<SubjectArg>>("onHappyEnd")
-                .OnNext(new SubjectArg("msg from legislator,happy end"));
-            }
-            else
-            {
-                GameEvents.Sigton.GetEvent<Subject<SubjectArg>>("onBadEnd")
-                .OnNext(new SubjectArg("msg from legislator,bad end"));
-            }
+            GameEnd();
         };
+
+        GameEvents.Sigton.timeSystem
+            .Where(x => x.DayCount == 16)
+            .Subscribe(x =>
+            {
+                GameEnd();
+            });
+    }
+    void GameEnd()
+    {
+        int coreIsland = IslandManager.Instance.m_islandSet.coreIslands.Count;
+        int normal = 0;
+        foreach (var item in IslandManager.Instance.m_islandSet.normalIslands)
+        {
+            if (item.isActive())
+            {
+                normal++;
+            }
+        }
+        int all = coreIsland + normal;
+        if (all >= 8)
+        {
+            GameEvents.Sigton.GetEvent<Subject<SubjectArg>>("onHappyEnd")
+            .OnNext(new SubjectArg("msg from legislator,happy end"));
+        }
+        else
+        {
+            GameEvents.Sigton.GetEvent<Subject<SubjectArg>>("onBadEnd")
+            .OnNext(new SubjectArg("msg from legislator,bad end"));
+        }
     }
     void Start()
     {
