c8e5a95385e2c5471f1af9730ed3ef2cf3f08db7
OldMoM <1090347251@qq.com>
2020-10-27 12:04:10 +0800

    打包测试

---
+++ Assets/LvPeixi/Test/ColliderTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderTest : MonoBehaviour
+{
+    MeshRenderer mesh;
+    Color[] colors = new Color[4];
+    int n = 0;
+    private void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mesh.material.color = Color.white;
+
+        colors[0] = Color.red;
+        colors[1] = Color.blue;
+        colors[2] = Color.green;
+        colors[3] = Color.yellow;
+    }
+    public void OnPlayerEnter()
+    {
+        n++;
+        if (n>=3)
+        {
+            n = 0;
+        }
+        mesh.material.color = colors[n];
+    }
+
+    public void OnPlayerExit()
+    {
+        mesh.material.color = Color.white;
+    }
+}
